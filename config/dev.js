440fc92d987d61b0bd0f55c4c1078b9dad806a1d
Shando Howell <93793381+shando-howell@users.noreply.github.com>
2024-12-14 10:21:14 -0500

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,1 @@
-module.exports = {
-  googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI:
-    'mongodb+srv://shandohowell94:cloudengineer55@blog.3dhqn.mongodb.net/blog?retryWrites=true&w=majority&appName=Blog',
-  cookieKey: '123123123'
-};
+
